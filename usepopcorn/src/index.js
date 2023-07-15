eeee0ba0ab7e7a064baf8a8b4d90b8d10f4389ad
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2023-07-15 23:45:44 +0300

    Building a reusable star component

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -2,10 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import StarRating from "./StarRating";
+import { useState } from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        maxRating={10}
+        onSetRating={setMovieRating}
+        size={24}
+        color="green"
+      />
+      <p>This movie has a rating of {movieRating} stars</p>
+    </div>
+  );
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} />
+    <StarRating
+      size={24}
+      color={"#fff"}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
